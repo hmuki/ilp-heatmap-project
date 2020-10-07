7839b8e656a2245ad1bb1fae18a377ddbb933e68
hoffmann-muki <s1894401@ed.ac.uk>
2020-10-08 00:14:35 +0100

    Finished section on generating a heat map.

--- heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
+++ heatmap/src/main/java/uk/ac/ed/inf/heatmap/App.java
@@ -21,10 +21,25 @@ import com.mapbox.geojson.FeatureCollection;
 
 public class App {
 
+	/**
+	 * Static method for reading all contents of an input file into a string
+	 * by means of its file path and file encoding format
+	 * 
+	 * @param path : the file path
+	 * @param encoding : the file encoding format 
+	 * @return a file object
+	 */
 	public static String readFile(String path, Charset encoding) throws IOException {
-		return Files.readString(Paths.get(path));
+		return Files.readString(Paths.get(path), encoding);
 	}
 
+	/**
+	 * Static method for converting the different ranges of air quality reading values
+	 * into hexadecimal RGB strings
+	 * 
+	 * @param value : the air quality reading value
+	 * @return RGB value as a hex string
+	 */
 	public static String rangeToRGBString(Integer value) {
 		if (value >= 0 && value < 32) {
 			return "#00ff00";
@@ -59,8 +74,11 @@ public class App {
 
 		String basePath = new File("").getAbsolutePath().concat("/");
 
+		/* 
+		 * Obtain the file path of the input file by appending the filename
+		 * to the path of the root directory
+		*/  
 		String filePath = basePath.concat(fileName);
-		System.out.println(filePath);
 
 		String textContent = null;
 
@@ -70,82 +88,84 @@ public class App {
 			System.out.println("An error occurred during the file reading.");
 		}
 
-		System.out.println(textContent);
 
 		// Split the strings over the commas
 		String[] readings = textContent.split(",");
 
-		// Convert to an array of integers
+		
 		ArrayList<Integer> values = new ArrayList<>();
 
+		/*
+		 * Convert the array of strings into an array of integers
+		 */
 		for (int i = 0; i < readings.length; i++) {
 			values.add(Integer.parseInt(readings[i].strip()));
 		}
+		
+		final int gridSize = 10;
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				System.out.print(values.get(10 * i + j) + " ");
-			}
-			System.out.println();
-		}
+		// Initialise constants 
 
-		System.out.println();
-		int gridSize = 10;
-
-		// Initialise constants
-
-		BigDecimal leftLongitude = BigDecimal.valueOf(-3.192473);
-		BigDecimal rightLongitude = BigDecimal.valueOf(-3.184319);
-		BigDecimal topLatitude = BigDecimal.valueOf(55.946233);
-		BigDecimal bottomLatitude = BigDecimal.valueOf(55.942617);
-		BigDecimal width = rightLongitude.subtract(leftLongitude);
-		BigDecimal height = topLatitude.subtract(bottomLatitude);
-		BigDecimal widthStepSize = width.divide(BigDecimal.valueOf(gridSize));
-		BigDecimal heightStepSize = height.divide(BigDecimal.valueOf(gridSize));
+		final BigDecimal leftLongitude = BigDecimal.valueOf(-3.192473);
+		final BigDecimal rightLongitude = BigDecimal.valueOf(-3.184319);
+		final BigDecimal topLatitude = BigDecimal.valueOf(55.946233);
+		final BigDecimal bottomLatitude = BigDecimal.valueOf(55.942617);
+		
+		/*
+		 * width represents the fixed distance between the leftmost and rightmost
+		 * longitudes that bound the grid
+		 */
+		final BigDecimal width = rightLongitude.subtract(leftLongitude);
+		/*
+		 * height represents the fixed distance between the topmost and bottom-most
+		 * latitudes that bound the grid
+		 */
+		final BigDecimal height = topLatitude.subtract(bottomLatitude);
+		
+		/*
+		 *  widthStepSize represents the constant width (difference between the longitudes) of
+		 *  each polygon in the grid
+		 */
+		final BigDecimal widthStepSize = width.divide(BigDecimal.valueOf(gridSize));
+		/*
+		 *  heightStepSize represents the constant height (difference between the latitudes) of
+		 *  each polygon in the grid
+		 */
+		final BigDecimal heightStepSize = height.divide(BigDecimal.valueOf(gridSize));
 
 		ArrayList<BigDecimal> horizontalPoints = new ArrayList<>();
 		ArrayList<BigDecimal> verticalPoints = new ArrayList<>();
 
-		// Compute evenly spaced points along the width (horizontal direction)
+		// Compute nine evenly spaced points along the width (horizontal direction)
 		for (int i = 1; i < gridSize; i++) {
 			BigDecimal point = leftLongitude.add(BigDecimal.valueOf(i).multiply(widthStepSize));
 			horizontalPoints.add(point);
 		}
 
-		// Compute evenly spaced points along the height (vertical direction)
+		// Compute nine evenly spaced points along the height (vertical direction)
 		for (int i = 1; i < gridSize; i++) {
 			BigDecimal point = topLatitude.subtract(BigDecimal.valueOf(i).multiply(heightStepSize));
 			verticalPoints.add(point);
 		}
 
-		for (BigDecimal e : horizontalPoints) {
-			System.out.println(e.round(new MathContext(8)));
-		}
-
-		System.out.println();
-
-		for (BigDecimal e : verticalPoints) {
-			System.out.println(e.round(new MathContext(8)));
-		}
-
-		System.out.println();
-
 		// Create an array representing the 10x10 grid
 		ArrayList<Polygon> grid = new ArrayList<>();
 
+		// Iterate through each row in the grid
 		for (int i = 0; i < gridSize; i++) {
-			// Compute top and bottom latitudes of a given rectangle
+			// Compute top and bottom latitudes of a given polygon
 			BigDecimal top = topLatitude.subtract(BigDecimal.valueOf(i).multiply(heightStepSize));
 			BigDecimal bottom = topLatitude.subtract(BigDecimal.valueOf(i + 1).multiply(heightStepSize));
 
+			// Iterate through each column in the grid
 			for (int j = 0; j < gridSize; j++) {
 				// ...and do likewise for the left and right longitudes
 				BigDecimal left = leftLongitude.add(BigDecimal.valueOf(j).multiply(widthStepSize));
 				BigDecimal right = leftLongitude.add(BigDecimal.valueOf(j + 1).multiply(widthStepSize));
 
 				/*
-				 * Create an array of Point objects that can later on be used to instantiate a
-				 * Polygon object.
+				 * Create an array of Point objects from the afore-named variables. The Point object is
+				 * used later on to instantiate a Polygon object.
 				 */
 				ArrayList<Point> points = new ArrayList<>();
 				points.add(Point.fromLngLat(left.doubleValue(), top.doubleValue()));
@@ -163,6 +183,11 @@ public class App {
 			}
 		}
 
+		/*
+		 * Initialise an array of features.
+		 * This array is used to store the feature properties of each polygon we've created.
+		 * The properties are processed from contents of the input file given to this application.
+		 */
 		ArrayList<Feature> features = new ArrayList<>();
 
 		for (int i = 0; i < grid.size(); i++) {
@@ -177,16 +202,20 @@ public class App {
 
 		FeatureCollection featureCollection = FeatureCollection.fromFeatures(features);
 
-		System.out.println(featureCollection.toJson());
+		
+		/*
+		 * Create a JSON file from the FeatureCollection object and write the output
+		 * geojson file to the default output directory, while catching any IO exceptions that
+		 * may arise from the operation.
+		 */
 		ArrayList<String> lines = new ArrayList<>();
 		lines.add(featureCollection.toJson());
-
-		// Write to text file for testing
 		Path outputFilePath = Paths.get(new File("").getAbsolutePath().concat("/heatmap.geojson"));
+		
 		try {
 		Files.write(outputFilePath, lines, StandardCharsets.UTF_8);
 		} catch (IOException e) {
-			System.out.println("THe given file cannot be created");
+			System.out.println("The given file cannot be created");
 		}
 	}
 }
